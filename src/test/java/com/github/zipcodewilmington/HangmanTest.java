e20280635403d9a654c06e97de950543e29f699e
zprongua <115650795+zprongua@users.noreply.github.com>
2022-11-10 15:26:47 -0500

    Tests added

--- src/test/java/com/github/zipcodewilmington/HangmanTest.java
+++ src/test/java/com/github/zipcodewilmington/HangmanTest.java
@@ -10,10 +10,32 @@ import org.junit.Test;
  */
 public class HangmanTest {
 
-    //Given
-    //When
-    //Then
+    @Test
+    public void testHangman() {
+        Hangman hm = new Hangman();
+        hm.initializeGameState();
+
+        Assert.assertEquals(3, hm.givenWord.length);
+    }
 
     @Test
-    
+    public void testDisplayWord() {
+        Hangman hm = new Hangman();
+        hm.initializeGameState();
+
+        String act = hm.printArray(hm.displayWord);
+        String exp = "_ _ _ ";
+        String given = hm.printArray(hm.givenWord);
+
+        Assert.assertEquals(exp, act);
+        Assert.assertNotEquals(act, given);
+    }
+
+    @Test
+    public void testWordGuessed() {
+        Hangman hm = new Hangman();
+        hm.initializeGameState();
+
+        Assert.assertFalse(hm.isWordGuessed());
+    }
 }
